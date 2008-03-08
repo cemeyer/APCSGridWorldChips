22af77e5e048fc4f77bc35edc932902821d2eaf6
Conrad Meyer <konrad@tylerc.org>
2008-03-07 16:28:44 -0800

    Only use gridworld's ant file now.

--- build.sh
+++ build.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-if [ "$1" = "-d" ]
-then
-  debug="-Xlint:unchecked"
-else
-  debug=""
-fi
-
-for dir in . gc; do
-  pushd $dir > /dev/null
-
-  if [ "`echo *.java`" != '*.java' ]
-  then
-    javac *.java $debug
-  fi
-
-  popd $dir > /dev/null
-done
-
-cd GridWorldCode
-ant compile
-cd ..
+pushd GridWorldCode > /dev/null
+ant
+popd > /dev/null
--- clean.sh
+++ clean.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-for dir in . gc; do
-  pushd $dir > /dev/null
-
-  if [ "`echo *.class`" != '*.class' ]
-  then
-    rm *.class
-  fi
-
-  popd $dir > /dev/null
-done
-
-cd GridWorldCode
+pushd GridWorldCode > /dev/null
 ant clean
-cd ..
+popd > /dev/null
