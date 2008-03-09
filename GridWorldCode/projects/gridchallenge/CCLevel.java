f90341a0bea82eb2a7343134dbd59c3e9071859f
Conrad Meyer <konrad@tylerc.org>
2008-03-08 21:53:13 -0800

    Modified Tile, CCLevel, and ignore build/ and dist/ in framework.

--- GridWorldCode/projects/gridchallenge/CCLevel.java
+++ GridWorldCode/projects/gridchallenge/CCLevel.java
@@ -1,6 +1,7 @@
 package gridchallenge;
 
 import java.util.LinkedList;
+import java.util.HashMap;
 
 public class CCLevel
 {
@@ -19,7 +20,7 @@ public class CCLevel
   private int levelNumber, timeLimit, numChips;
 
   // x, y, layer
-  private byte[][][] map;
+  private int[][][] map;
 
   private byte[] leveldata;
   private int idx = 0;
@@ -27,37 +28,42 @@ public class CCLevel
   private String title, hint, password;
 
   private LinkedList<Point> movingMonsters = new LinkedList<Point>();
+  private HashMap<Point, Point> trapControls = new HashMap<Point, Point>();
+  private HashMap<Point, Point> cloneControls = new HashMap<Point, Point>();
 
   public CCLevel(byte[] leveldata)
   {
     // setup map data
-    map = new byte[32][][];
+    map = new int[32][][];
     for (int i = 0; i < 32; i++)
     {
-      map[i] = new byte[32][];
-      map[i][0] = new byte[2];
-      map[i][1] = new byte[2];
+      map[i] = new int[32][];
+      for (int j = 0; j < 32; j++)
+        map[i][j] = new int[2];
     }
 
     this.leveldata = leveldata;
 
-    parseLevelData();
+    try { parseLevelData(); } catch(ArrayIndexOutOfBoundsException ex) {}
   }
 
-  private void parseLevelData()
+  private void parseLevelData() throws ArrayIndexOutOfBoundsException
   {
+    final boolean debug = false;
     levelNumber = readInt16();
     timeLimit   = readInt16();
     numChips    = readInt16();
+    if (debug) System.out.println("Read: 6 bytes");
 
     boolean breakFree = false;
     for (;;)
     {
       int field = readInt8();
+      int len = readInt8();
       switch (field)
       {
         case 1:
-          readInt8(); // get rid of second byte in fieldtype word
+          if (debug) System.out.println("Field 1:");
           int sizeFirstLayer = readInt16();
           int[] firstlayer = readInts8(sizeFirstLayer);
 
@@ -68,49 +74,105 @@ public class CCLevel
           copyLayerIntoMap(1, secondlayer);
 
           int bytesRemaining = readInt16();
+          if (debug) System.out.println("  Read: " + (8 + sizeFirstLayer + sizeSecondLayer) + " bytes");
           break;
         case 3:
-          int titleLen = readInt8();
-          title = readString(titleLen);
-          System.out.println(title);
+          if (debug) System.out.println("Field 3:");
+          title = readString(len);
+          if (debug) System.out.println("  Title: " + title);
+          if (debug) System.out.println("  Read: " + (2 + len) + " bytes");
+          break;
+        case 4:
+          if (debug) System.out.println("Field 4:");
+          int[] trapcontrols = readInts8(len);
+          handleTrapControls(trapcontrols);
+          break;
+        case 5:
+          if (debug) System.out.println("Field 5:");
+          int[] cloningcontrols = readInts8(len);
+          handleCloningControls(cloningcontrols);
           break;
         case 6:
-          int passlen = readInt8(); // always 5...
-          password = convertPassword(readString(passlen));
-          System.out.println(password);
+          if (debug) System.out.println("Field 6:");
+          password = convertPassword(readString(len));
+          if (debug) System.out.println("  Password: " + password);
+          if (debug) System.out.println("  Read: " + (2 + len) + " bytes");
           break;
         case 7:
-          int hintlen = readInt8();
-          hint = readString(hintlen);
-          System.out.println(hint);
+          if (debug) System.out.println("Field 7:");
+          hint = readString(len);
+          if (debug) System.out.println("  Hint: " + hint);
+          if (debug) System.out.println("  Read: " + (2 + len) + " bytes");
           break;
         case 10:
+          if (debug) System.out.println("Field 10:");
           // monster motion
-          for (;;)
+          for (int m = 0; m < len; m += 2)
           {
-            if (idx >= leveldata.length)
-            {
-              breakFree = true;
-              break;
-            }
             int x = readInt8();
             int y = readInt8();
             movingMonsters.add(new Point(x, y));
+            if (debug) System.out.println("  Monster: " + x + ", " + y);
           }
           break;
         default:
           throw new RuntimeException("We ought to handle field type " + field);
       }
-      if (breakFree) break;
-      if (idx >= leveldata.length) break;
+    }
+  }
+
+  private void handleCloningControls(int[] ctrls)
+  {
+    for (int i = 0; i < ctrls.length; i += 8)
+    {
+      cloneControls.put(
+          new Point(ctrls[i + 0] + 256 * ctrls[i + 1],
+            ctrls[i + 2] + 256 * ctrls[i + 3]),
+          new Point(ctrls[i + 4] + 256 * ctrls[i + 5],
+            ctrls[i + 6] + 256 * ctrls[i + 7])
+          );
+    }
+  }
+
+  private void handleTrapControls(int[] ctrls)
+  {
+    for (int i = 0; i < ctrls.length; i += 10)
+    {
+      trapControls.put(
+          new Point(ctrls[i + 0] + 256 * ctrls[i + 1],
+            ctrls[i + 2] + 256 * ctrls[i + 3]),
+          new Point(ctrls[i + 4] + 256 * ctrls[i + 5],
+            ctrls[i + 6] + 256 * ctrls[i + 7])
+          );
+      // last two bytes ([i + 8] [i + 9]) are terminating nulls.
     }
   }
 
   private void copyLayerIntoMap(int elevation, int[] rleData)
   {
+    int outidx = 0;
+    for (int i = 0;;)
+    {
+      switch(rleData[i])
+      {
+        case 0xFF:
+          int copies = rleData[i + 1] + outidx;
+          int ofwhat = rleData[i + 2];
+          for (; outidx < copies; outidx++)
+            map[outidx & 0x1F][outidx >> 5][elevation] = ofwhat;
+          i += 3;
+          break;
+        default:
+          map[outidx & 0x1f][outidx >> 5][elevation] = rleData[i];
+          i++;
+          outidx++;
+          break;
+      }
+      if (i >= rleData.length) break;
+    }
   }
 
-  private String readString(int len)
+  private String readString(int len) throws ArrayIndexOutOfBoundsException
   {
     int[] nums = readInts8(len);
     char[] res = new char[len - 1];
@@ -121,21 +183,21 @@ public class CCLevel
     return new String(res);
   }
 
-  private int[] readInts8(int count)
+  private int[] readInts8(int count) throws ArrayIndexOutOfBoundsException
   {
     int[] res = new int[count];
     for (int i = 0; i < count; i++) res[i] = readInt8();
     return res;
   }
 
-  private int readInt16()
+  private int readInt16() throws ArrayIndexOutOfBoundsException
   {
     int lsb = readInt8();
     int msb = readInt8();
     return msb * 256 + lsb;
   }
 
-  private int readInt8()
+  private int readInt8() throws ArrayIndexOutOfBoundsException
   {
     int r = leveldata[idx] & 0xFF;
     idx++;
@@ -144,9 +206,12 @@ public class CCLevel
 
   private String convertPassword(String pass)
   {
-    byte[] bs = pass.getBytes();
-    for (int i = 0; i < bs.length; i++) bs[i] ^= 0xC3;
-    return new String(bs);
+    char[] cs = pass.toCharArray();
+    for (int i = 0; i < cs.length; i++)
+    {
+      cs[i] ^= 0x99;
+    }
+    return new String(cs);
   }
 
   public CCLevel getNext()
