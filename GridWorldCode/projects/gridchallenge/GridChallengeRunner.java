7421444d3986f3b0c91214f02802aece63234497
Conrad Meyer <konrad@tylerc.org>
2008-03-12 13:41:13 -0700

    Added CC revised original levels as an option for play.

--- GridWorldCode/projects/gridchallenge/GridChallengeRunner.java
+++ GridWorldCode/projects/gridchallenge/GridChallengeRunner.java
@@ -3,6 +3,7 @@ package gridchallenge;
 import info.gridworld.actor.ActorWorld;
 
 import java.io.IOException;
+import javax.swing.JOptionPane;
 
 public class GridChallengeRunner
 {
@@ -11,8 +12,22 @@ public class GridChallengeRunner
   public static void main(String[] args){
     try
     {
+      String choice = "/CCRL/ccrev.dat";
+      Object[] options = {"CCLP2", "CC Revised"};
+      if (JOptionPane.showOptionDialog(null,
+            "What levelset do you want to play?",
+            "Levelset chooser",
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            options,
+            options[1]) == 0)
+      {
+        choice = "/CCLP2/CCLP2.dat";
+      }
+
       c = new CCLevelSet(System.getProperty("user.dir") +
-          "/gridchallenge/CCLP2/CCLP2.dat");
+          "/gridchallenge" + choice);
 
       CCWorld world = new CCWorld(c.getLevel(1));
 
